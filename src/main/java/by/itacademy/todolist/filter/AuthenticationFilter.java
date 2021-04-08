8c3f520a1937edc9241233f706cf394909a58d1b
KAKORA_ALIAKSEI <akakora@yandex.by>
2021-04-08 23:20:05 +0300

    changed AuthenticationFilter (changed pages lists)

--- src/main/java/by/itacademy/todolist/filter/AuthenticationFilter.java
+++ src/main/java/by/itacademy/todolist/filter/AuthenticationFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,12 +16,11 @@ import java.util.List;
 @WebFilter (urlPatterns = "/*", dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD})
 public class AuthenticationFilter implements Filter {
 
+    private final List<String> guestPagesList
+            = Arrays.asList("/index.jsp", "/login.jsp", "/registration.jsp", "/blocked.jsp", "/guest");
 
-    private String [] guestPages = {"/index.jsp", "/login.jsp", "/registration.jsp", "/blocked.jsp", "/guest"};
-    private String [] adminPages = {"/adminPage.jsp", "/allUsers.jsp", "/messageCard.jsp", "/userCard.jsp", "/allMessages.jsp"};
-
-    List<String> guestPagesList = new ArrayList<>(Arrays.asList(guestPages));
-    List<String> adminPagesList = new ArrayList<>(Arrays.asList(adminPages));
+    private final List<String> adminPagesList
+            = Arrays.asList("/adminPage.jsp", "/allUsers.jsp", "/messageCard.jsp", "/userCard.jsp", "/allMessages.jsp");
 
     @Override
     public void init(FilterConfig filterConfig) {
